6f0f826afce6ccc19a45988db3d29f83e67c1a87
Jorge Retana <ippofo@gmail.com>
2022-05-29 18:44:15 +0200

    maquetacion lista partidos boton borrar

--- src/ComponenteListPartidos.js
+++ src/ComponenteListPartidos.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { db } from './firebase-config'
-import { collection, getDocs, updateDocs, doc, deleteDoc } from 'firebase/firestore'
+import './listaPartidos.css';
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  updateDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const ComponenteListPartidos = (props) => {
+  const partidosCollectionRef = collection(db, "partidos");
 
-  const partidosCollectionRef = collection(db, 'partidos');
-
+  
   const updateMatch = async (id, organizador) => {
-    console.log('id', id);
-    console.log('organizador', organizador);
-    const matchDoc = doc(db, 'partidos', id)
+    console.log("id", id);
+    console.log("organizador", organizador);
+    const matchDoc = doc(db, "partidos", id);
     //const newFields = { organizador: "organizador" }
     await updateMatch(matchDoc, {
-      organizador: 'organizador'
-    })
-  }
+      organizador: "organizador",
+    });
+  };
   const deleteUser = async (id) => {
     const matchDoc = doc(db, "partidos", id);
     await deleteDoc(matchDoc);
@@ -23,45 +30,55 @@ const ComponenteListPartidos = (props) => {
 
   return (
     <div className="justify-content-center">
-      <div className="mb-5">
-        <h2>Próximos partidos</h2>
+      <h2>Próximos partidos</h2>
 
-        {/* card */}
-        {props.list.length > 0 ? (
-          <div>
-            {props.list.map((item, index) => {
-              return (
-                <div className="card text-center m-2 border-0">
-                  <div className="card-body">
-                    <div className="card-header" key={index} >PARTIDO</div>
-                    <div className="card-body carta">
-                      <h5 className="card-title">
-                        Organizador: {item.organizador} - Integrantes:{" "}
-                        {(item.equipo).length}/10
-                      </h5>
-                    </div>
-                    <div className="card-footer text-muted">
-                      Fecha: {item.dia} - Hora:{item.hora}
-                    </div>
-                    <div className="card-footer text-muted">
+      {/* card */}
+      {props.list.length > 0 ? (
+        <div>
+          {props.list.map((item, index) => {
+            return (
+              <div className="card text-center m-2 border-0">
+                <div className="card-body">
+                  <div className="card-header" key={index}>
+                    PARTIDO
+                  </div>
+                  <div className="card-body carta">
+                    <h5 className="card-title">
+                      Organizador: {item.organizador} - Integrantes:{" "}
+                      {item.equipo.length}/10
+                    </h5>
+                  </div>
+                  <div className="card-footer text-muted">
+                    Fecha: {item.dia} - Hora: {item.hora}
+                  </div>
+                  <div className="card-footer text-muted">
                     <button
+                      class="noselect delete"
+                    >
+                      <span class="text">Borrar</span>
+                      <span class="icon">
+                        <svg
                         onClick={() => {
                           deleteUser(item.id);
                         }}
-                      >
-                        {" "}
-                        Borrar partido
-                      </button>
-                    </div>
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                        </svg>
+                      </span>
+                    </button>
                   </div>
                 </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div className="alert">No hay partidos añadidos</div>
-        )}
-      </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="alert">No hay partidos añadidos</div>
+      )}
     </div>
   );
 };
--- src/MainApp.js
+++ src/MainApp.js
@@ -1,23 +1,29 @@
 /* eslint-disable react/jsx-no-undef */
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navegacion from './Navegacion';
-import ComponenteAddMatch from './ComponenteAddMatch';
-import ComponenteListPartidos from './ComponenteListPartidos';
-import { db } from './firebase-config'
-import { collection, getDocs, updateDocs, doc, deleteDoc } from 'firebase/firestore'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.css";
+import Navegacion from "./Navegacion";
+import ComponenteAddMatch from "./ComponenteAddMatch";
+import ComponenteListPartidos from "./ComponenteListPartidos";
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  updateDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy"
+  crossorigin="anonymous"
+></script>;
 
-
-
-
-const MainApp = props => {
-
-
-  console.log('props', props);
+const MainApp = (props) => {
+  console.log("props", props);
   const [partidos, setPartidos] = useState([]);
-  console.log('partidos', partidos);
-  const partidosCollectionRef = collection(db, 'partidos');
+  console.log("partidos", partidos);
+  const partidosCollectionRef = collection(db, "partidos");
 
   // const updateMatch = async (id, organizador) => {
   //   console.log('id', id);
@@ -37,16 +43,15 @@ const MainApp = props => {
     const getPartidos = async () => {
       const data = await getDocs(partidosCollectionRef);
       setPartidos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    }
+    };
     getPartidos();
-  }, [])
+  }, []);
   const [visualizacion, setVisualizacion] = useState({
     componenteAddMatch: true,
     // componenteTodos: true,
     // componenteFiltro: false,
   });
   return (
-    
     <div className="App container">
       {/* {partidos.map((partido, index) => {
         return (
@@ -75,23 +80,41 @@ const MainApp = props => {
       })} */}
       {/* <Navegacion visualizacion={setVisualizacion} /> */}
       <div className="container mt-4">
-        <h1>Aplicación de gestión de Partidos y torneos de Volleyball</h1>
+        <h1>SPORT-MENTS</h1>
         <h2>El número de partidos actuales es de: {partidos.length}</h2>
+        <div
+          id="carouselExampleSlidesOnly"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="100">
+              <img src="https://cdn.pixabay.com/photo/2021/07/20/14/06/sport-6480830_960_720.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="https://cdn.pixabay.com/photo/2021/07/20/14/06/sport-6480830_960_720.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="..." />
+            </div>
+          </div>
+        </div>
+        <hr></hr>
         <div className="row mt-4">
-          <div className="col-10">
+          <div className="col-12">
             <ComponenteListPartidos list={partidos} />
           </div>
         </div>
         <div className="row mt-4">
-          <div className="col-8">
+          <div className="col-12">
             <ComponenteAddMatch setMatch={setPartidos} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-MainApp.propTypes = {}
+MainApp.propTypes = {};
 
-export default MainApp
+export default MainApp;
