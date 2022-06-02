fdc8eed169992f36e96a66538365fbe9bacc565e
Juan J. Barriga García <jubaga95@gmail.com>
2022-06-02 19:55:53 +0200

    prueba por si se rompe

--- src/MainApp.js
+++ src/MainApp.js
@@ -3,7 +3,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ComponenteAddMatch from "./ComponenteAddMatch";
 import ComponenteListPartidos from "./ComponenteListPartidos";
-import Footer from "./Footer";
+import Footer from "./Footer"; 
 <script
   src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy"
