084b55faf2cab02c5b4efd13e43851e82462a81c
Jorge Retana <ippofo@gmail.com>
2022-06-09 12:57:15 +0200

    torneo component acabado

--- src/pages/TournamentComponent.js
+++ src/pages/TournamentComponent.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import "./torneo.css"
 function TournamentComponent(props) {
   return (
     <div className="container mt-5">
-       <img src="https://cdn-icons-png.flaticon.com/512/5578/5578536.png" className="text-center"></img>
+       <img src="https://cdn-icons-png.flaticon.com/512/5578/5578536.png" className="text-center" id="work"></img>
     </div>
    
   )
